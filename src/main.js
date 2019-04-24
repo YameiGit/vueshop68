1231a2f5ac3f9c3ceae13f8f930353e4578dc011
chenyamei <2394097594@qq.com>
2019-04-24 21:54:41 +0800

    后台创建完毕

--- src/main.js
+++ src/main.js
@@ -8,6 +8,20 @@ import ElementUI from 'element-ui'
 Vue.use(ElementUI)
 
 axios.defaults.baseURL = 'http://127.0.0.1:11333/api/private/v1/'
+axios.interceptors.request.use(
+  function(config) {
+    console.dir(config)
+    // 给axios配置token
+    var token = window.sessionStorage.getItem('token')
+    if (token !== null) {
+      config.headers.Authorization = token
+    }
+    return config
+  },
+  function(error) {
+    return Promise.reject(error)
+  }
+)
 Vue.prototype.$http = axios
 // Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,25 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
 import Home from '@/components/Home'
+import Welcome from '@/components/Welcome'
 Vue.use(Router)
 const router = new Router({
   routes: [
-    {path: '/', redirect: '/login'},
-    {path: '/login', component: Login},
-    {path: '/home', component: Home}
+    { path: '/login', component: Login },
+    { path: '/', redirect: '/welcome' },   // 默认执行welcome路由
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',       // 默认执行welcome路由
+      children: [
+        { path: '/welcome', component: Welcome },
+      ]
+    }
   ]
 })
-router.beforeEach((to, from, next) => {
-  const token = window.sessionStorage.getItem('token')
+ router.beforeEach((to, from, next) => {
+  
+  var token = window.sessionStorage.getItem('token')
   if (token === null && to.path !== '/login') {
     return next('/login')
   }
